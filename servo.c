4e9a62c1899f86ace76ca1eefc14539dc063c5f1
Simu3 <simu3.developer@gmail.com>
2015-05-06 14:55:21 +0900

    send kondo servo data and add delay

--- servo.c
+++ servo.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/time.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -9,12 +10,16 @@
 #include <string.h>
 
 #define DEVICE   "/dev/cu.usbserial-FTH7PKJS"
-#define BAUDRATE B9600
+#define BAUDRATE B115200
+#define ID       0
 
 int main(void){
     int fd;
     struct termios oldtio, newtio;
-    char data;
+    struct timespec ts;
+    int data = 7500;
+    unsigned char cmd;
+    unsigned char pos_h, pos_l;
 
     fd = open(DEVICE, O_RDWR | O_NOCTTY | O_NONBLOCK);
 
@@ -29,9 +34,23 @@ int main(void){
     tcflush(fd, TCIFLUSH);
     tcsetattr(fd, TCSANOW, &newtio);
 
-    data = 97;
-    write(fd, &data, sizeof(data));
+    /* User's program */
+    ts.tv_sec = 0;
+    ts.tv_nsec = 10000000;
+
+    cmd = 0x80 | ID;
+    pos_h = (unsigned char)((data >> 7) & 0x7F);
+    pos_l = (unsigned char)(data & 0x7F);
+    write(fd, &cmd, sizeof(cmd));
+    nanosleep(&ts, NULL);
     printf("write\n");
+    write(fd, &pos_h, sizeof(pos_h));
+    nanosleep(&ts, NULL);
+    printf("write\n");
+    write(fd, &pos_l, sizeof(pos_l));
+    nanosleep(&ts, NULL);
+    printf("write\n");
+    /* end */
 
     tcsetattr(fd, TCSANOW, &oldtio);
     close(fd);
